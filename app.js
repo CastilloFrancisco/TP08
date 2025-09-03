29f6b978c59114ee6be152e061a97ec8a575ab52
Fran <francisco.castillo2704@gmail.com>
2025-09-03 01:11:11 -0300

    aaaaaaaaaaaaaaaa

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
-function operacion() {
-  const num1 = parseFloat(document.getElementById("n1").value);
-  const num2 = parseFloat(document.getElementById("n2").value);
-  const op = document.getElementById("operación");
+function actualizarNum(num) {
+
   const resultado = document.getElementById("resultado");
 
-  if (op.value === "+") {
-    resultado.innerHTML = num1 + num2;
-  } else if (op.value === "-") {
-    resultado.innerHTML = num1 - num2;
-  } else if (op.value === "*") {
-    resultado.innerHTML = num1 * num2;
-  } else if (op.value === "/") {
-    resultado.innerHTML = num1 / num2;
+  if (resultado.innerHTML === "Resultado") {
+
+    resultado.innerHTML = num;
+
+  } else {
+
+    resultado.innerHTML += num;
+  }
+}
+
+function agregarSimbolo(simbolo) {
+
+  const resultado = document.getElementById("resultado");
+
+
+  if ((resultado.innerHTML === "Resultado" || resultado.innerHTML === "Error" || resultado.innerHTML === "+" || resultado.innerHTML === "-") && (simbolo === "+" || simbolo === "-")) {
+
+    resultado.innerHTML = simbolo;
+
+  } else if (resultado.innerHTML === "Resultado" || resultado.innerHTML === "Error" || resultado.innerHTML === "+" || resultado.innerHTML === "-") {
+
+    resultado.innerHTML = "Error"
+
+  } else if ((resultado.innerHTML).slice(-1) !== "+" && (resultado.innerHTML).slice(-1) !== "-"){
+
+    resultado.innerHTML += simbolo
+
+  } else {
+
+    resultado.innerHTML = (resultado.innerHTML).slice(0, -1)
+    resultado.innerHTML += simbolo
+  }
+}
+
+function borrar() {
+  const resultado = document.getElementById("resultado");
+
+  if (resultado.innerHTML !== "Resultado" && resultado.innerHTML !== "Error") {
+
+    resultado.innerHTML = (resultado.innerHTML).slice(0, -1)
+
+    if (resultado.innerHTML === "") {
+
+      resultado.innerHTML = "Resultado"
+    
+    }
+
   }
 }
